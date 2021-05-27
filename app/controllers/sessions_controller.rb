f335ddb09f565c218c615ba0c680dff09f4ab41d
Kiup Kim <kukim96@gmail.com>
2021-05-27 17:52:06 -0600

    finishing up user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id 
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
       redirect_to :root
     else
       render :new
