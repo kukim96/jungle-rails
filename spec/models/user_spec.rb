a00c6d44fd2cea7babd35eab518b83fee17670e8
Kiup Kim <kukim96@gmail.com>
2021-05-31 16:16:46 -0600

    switched to subject and added test to see if there are first name, last name, email, password, and password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject {
+    described_class.new(first_name: "Daniel", last_name: "Kim", email: "kukim96@gmail.com", password: "supersecret", password_confirmation: "supersecret")
+  }
   describe "Validations" do
     it "is valid with valid attributes" do
-      user = User.create(first_name: "Daniel", last_name: "Kim", email: "kukim96@gmail.com", password: "supersecret", password_confirmation: "supersecret")
-      expect(user).to be_valid
-      expect(user.errors.full_messages).to be_empty
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
     end
 
     it "is not valid if password and password_confirmation don't match each other" do
-      user = User.create(first_name: "Lighthouse", last_name: "Labs", email: "lighthouselabs@gmail.com", password: "testpw", password_confirmation: "testpw1")
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      subject.password_confirmation = "superdupersecret"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
 
     it "is not valid when email isn't unique (not case sensitive)" do
-      user = User.create(first_name: "Hello", last_name: "World", email: "helloworld@gmail.com", password: "secretpw1", password_confirmation: "secretpw1")
-      same_user = User.create(first_name: "Hello", last_name: "World", email: "HelloWorld@gmail.com", password: "secretpw1", password_confirmation: "secretpw1")
-      expect(same_user).to_not be_valid
-      expect(same_user.errors.full_messages).to include "Email has already been taken"
+      same_as_subject = User.create(first_name: "Daniel", last_name: "Kim", email: "KUKIM96@gmail.com", password: "supersecret", password_confirmation: "supersecret")
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Email has already been taken"
+    end
+
+    it "is not valid without first name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "First name can't be blank"
+    end
+
+    it "is not valid without last name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Last name can't be blank"
+    end
+
+    it "is not valid without email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Email can't be blank"
+    end
+
+    it "is not valid without password" do 
+      subject.password = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Password can't be blank"
+    end
+
+    it "is not valid without password confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages). to include "Password confirmation can't be blank"
     end
   end
 end
